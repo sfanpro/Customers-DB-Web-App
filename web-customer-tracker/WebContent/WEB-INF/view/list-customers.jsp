83ca94a6b64a501e12a14385614e4337a6029a13
sfanpro <st.sergeyf@gmail.com>
2019-04-14 01:31:22 +0300

    developing JSP view page

--- web-customer-tracker/WebContent/WEB-INF/view/list-customers.jsp
+++ web-customer-tracker/WebContent/WEB-INF/view/list-customers.jsp
@@ -1,3 +1,5 @@
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
+
 <!DOCTYPE html>
 
 <html>
@@ -8,8 +10,34 @@
 
 <body>
 
-List customers
+	<div id="wrapper">
+		<div id="header">
+			<h2>CRM - Customer Relationship Manager</h2>
+		</div>
 
+		<div id="container">
+			<div id="content">
+
+				<!-- Add our html table here -->
+				<table>
+					<tr>
+						<th>First Name</th>
+						<th>Last Name</th>
+						<th>Email</th>
+					</tr>
+
+				<!-- loop over and print our customers -->
+				<c:forEach var="tempCustomers" items="${customers}">
+					<tr>
+						<td>${tempCustomers.firstName}</td>
+						<td>${tempCustomers.lastName}</td>
+						<td>${tempCustomers.email}</td>
+					</tr>
+				</c:forEach>
+				</table>
+			</div>
+		</div>
+	</div>
 </body>
 
 </html>
--- web-customer-tracker/src/com/ua/team/springdemo/controller/CustomerController.java
+++ web-customer-tracker/src/com/ua/team/springdemo/controller/CustomerController.java
@@ -1,15 +1,31 @@
 package com.ua.team.springdemo.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.ua.team.springdemo.dao.CustomerDAO;
+import com.ua.team.springdemo.entity.Customer;
 
 @Controller
 @RequestMapping("/customer")
 public class CustomerController {
 
+	// need to inject the customer dao
+	@Autowired
+	private CustomerDAO customerDAO;
+
 	@RequestMapping("/list")
 	public String listCustomers(Model theModel) {
+
+		// get customers from the DAO
+		List<Customer> theCustomers = customerDAO.getCustomers();
+
+		// add customer to the model
+		theModel.addAttribute("customers", theCustomers);
 
 		return "list-customers";
 	}
